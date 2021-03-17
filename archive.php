72945b972f5975b86c0dea8d8d6ead4b1169da36
Alit <alitwarasto@gmail.com>
2021-03-17 17:28:25 +0700

    archive php awal

--- archive.php
+++ archive.php
@@ -2,6 +2,34 @@
 #archive.php for pantiespizza.com
 #saycheese theme wp
 #version 1.0
+
+if(is_category()) {
+  $ptitle = $wp_query->queried_object->name;
+  $plink  = $siteurl.'/category/'.$wp_query->queried_object->slug .'/';
+} elseif(is_tag()) {
+  $ptitle = ucwords($wp_query->queried_object->name);
+  $plink  = $siteurl.'/tag/'.$wp_query->queried_object->slug .'/';
+} elseif(is_day()) {
+  $ptitle = date('F', mktime(0,0,0,$wp_query->query_vars['monthnum'],1,$wp_query->query_vars['year'])).' '.$wp_query->query_vars['day'].', '.$wp_query->query_vars['year'].' Archive';
+  $plink  = $siteurl.'/'.$wp_query->query_vars['year'].'/'.$wp_query->query_vars['monthnum'].'/'.$wp_query->query_vars['day'].'/';
+} elseif(is_month()) {
+  $ptitle = date('F', mktime(0,0,0,$wp_query->query_vars['monthnum'],1,$wp_query->query_vars['year'])).', '.$wp_query->query_vars['year'].' Archive';
+  $plink  = $siteurl.'/'.$wp_query->query_vars['year'].'/'.$wp_query->query_vars['monthnum'].'/';
+} elseif(is_year()) {
+  $ptitle = $wp_query->query_vars['year'].' Archive';
+  $plink  = $siteurl.'/'.$wp_query->query_vars['year'].'/';
+} elseif(is_author()) {
+  $authname = $wp_query->queried_object->data->display_name;
+  $ptitle   = $wp_query->queried_object->data->display_name.'&#8217;s Articles';
+  $plink    = $siteurl.'/author/'.$wp_query->query_vars['author_name'].'/';
+} else {
+  $ptitle = '';
+  $plink  = '';
+}
+
+/* Random Query 
+$offset = mt_rand(1,($allposts-7));
+$rq     = new WP_Query('posts_per_page=7');*/
 ?>
 
 <?php
@@ -29,6 +57,7 @@ if ( $detect->isMobile() && !$detect->isTablet() ){
           the_post_thumbnail('Lthumb', array( 'class' => 'img-fluid mb-2' ));
         	}
         endwhile;
+        wp_reset_postdata();
       endif;
        ?>
     </div>
