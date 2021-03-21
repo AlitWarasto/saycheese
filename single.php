182f881183a970cb03a7cd99358194cd73df09af
Alit <alitwarasto@gmail.com>
2021-03-21 19:55:32 +0700

    attachment single blm selesai

--- single.php
+++ single.php
@@ -19,23 +19,11 @@ if ( $detect->isMobile() && !$detect->isTablet() ){
     <?php while (have_posts()) : the_post();?>
       <h1 class="col-12 text-center"><?php the_title(); ?></h1>
       <?php
-      $args = array(
-            'numberposts' => -1,
-            'orderby' => 'date',
-            'post_mime_type' => 'image',
-            'post_parent' => $post->ID,
-            'post_status' => null,
-            'post_type' => 'attachment',
-          );
-      $atts = get_children($args);
-      foreach ($atts as $att) {
-      $posurl  = $siteurl.'/'.$post->post_name.'/';
-      $aturl  = $posurl.$att->post_name.'/';
-      }
       $mfi = kdmfi_get_featured_image_id('mfi');
       $imfi = wp_get_attachment_image_src($mfi, 'Lthumb');
+      $imgl = get_attachment_link($mfi);
       ?>
-      <a href="<?php echo $aturl; ?>"><img src="<?php echo $imfi[0]; ?>" alt="<?php the_title(); ?>" class="img-fluid mb-2"/></a>
+      <a href="<?php echo esc_url ($imgl); ?>"><img src="<?php echo $imfi[0]; ?>" alt="<?php the_title(); ?>" class="img-fluid mb-2"/></a>
       <div class="col-12"><?php the_content(); ?></div>
       <a href="<?php echo get_day_link(get_post_time('Y'), get_post_time('m'), get_post_time('j'));  ?>">
         <div class="col-12" style="font-size:x-small;color: #8e8e8e;">Publised <?php the_time('F j, Y') ?></div>
