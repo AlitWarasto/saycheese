3c8d2308ca79c0960251bb758d7edd7034001f70
AlitWarasto <alitwarasto@gmail.com>
2022-02-03 16:06:40 +0700

    saycheese kemitraan 1

--- pages/page-kemitraan.php
+++ pages/page-kemitraan.php
@@ -27,20 +27,39 @@ include(TEMPLATEPATH.'/header.php'); ?>
 			<h1>Good Story</h1>
 			<h6>New Taste for Pizza</h6>
 			<p>Panties Pizza adalah The Leader of Melting Cheese Pizza. 
-Panties Pizza
-Indonesia, berdiri sejak 3 Mei 2013 di kota Surakarta. Kini Panties Pizza telah
-memiliki lebih dari 50 cabang outlet di seluruh Indonesia. Seiring dengan
-berjalannya waktu, Panties Pizza berkomitmen untuk selalu memberikan
-pelayanan, inovasi dan kualitas produk yang terbaik kepada konsumen
-setianya. 
-Berbagai varian menu, seperti Say Cheese, Pizzaderman, Mission
-Im Pizzable, University Pizza selalu siap menjadi mood booster anda
-kapanpun. Tidak kalah juga varian menu terbaru seperti Mixzone
-Pizza dan Pizza Slice.</p>
+			Panties Pizza
+			Indonesia, berdiri sejak 3 Mei 2013 di kota Surakarta. Kini Panties Pizza telah
+			memiliki lebih dari 50 cabang outlet di seluruh Indonesia. Seiring dengan
+			berjalannya waktu, Panties Pizza berkomitmen untuk selalu memberikan
+			pelayanan, inovasi dan kualitas produk yang terbaik kepada konsumen
+			setianya. 
+			Berbagai varian menu, seperti Say Cheese, Pizzaderman, Mission
+			Im Pizzable, University Pizza selalu siap menjadi mood booster anda
+			kapanpun. Tidak kalah juga varian menu terbaru seperti Mixzone
+			Pizza dan Pizza Slice.</p>
 			<span>„</span>
 		</div>
 		<div class="col-md-6">
 			<img id="bgh2" class="img-fluid" src="<?php echo $siteurl ?>/wp-content/uploads/2022/02/franchise-panties-pizza-peluang-usaha-yang-belum-banyak-pesaing-2022.png">
+		</div>
+	</div>
+</section>
+<section id="kemitraan-halaman-3" class="container bgPink">
+	<div class="row">
+		<div class="col-md-4">
+			<h1>Cerita Sukses Mitra Panties</h1>
+			<img id="bgfirman" class="img-fluid" src="<?php echo $siteurl ?>/wp-content/uploads/2022/02/testimoni-bapak-firman-salah-satu-mitra-panties-pizza-purwokerto.png">
+			<h4>Bp. Firman A. B</h4>
+			<h5>Mitra Panties Pizza Purwokerto</h5>
+		</div>
+		<div class="col-md-8">
+			<p>Dulu waktu awal Panties Pizza buka, tidak lama dari itu saya mengajukan kemitraan Panties Pizza dan bertemu langsung dengan owner Panties Pizza. Tapi saat
+			itu mereka belum membuka Kemitraan.
+			Sekarang setelah bergabung menjadi Mitra Panties Pizza di Kota Purwokerto. Alhamdulillah bisnis ini berkembang pesat dan dapat diterima oleh semua kalangan customer di kota ini.
+			Semua ini dapat terwujud karena dukungan dari tim pusat, mulai dari program marketing, SOP menu, pengiriman bahan baku yang tepat waktu dan maintenance outlet
+			yang saya lakukan secara baik agar brand Panties Pizza di Kota Purwokerto tetap terpercaya oleh customer.
+			Terima kasih Panties Pizza Indonesia</p>
+			<img id="bgh3" class="img-fluid" src="<?php echo $siteurl ?>/wp-content/uploads/2022/02/usaha-kuliner-yang-menjanjikan-disaat-pandemi-2022.png">
 		</div>
 	</div>
 </section>
